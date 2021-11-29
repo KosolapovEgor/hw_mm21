5e47082ff695b3ccd3e3c164f10c2e704129788c
Egor <90556524+KosolapovEgor@users.noreply.github.com>
2021-11-30 02:22:54 +0300

    Update main.cpp

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -1,130 +1,78 @@
-﻿#include <iostream>
-#include <string>
+#include <iostream>
+#include <cmath>
 #include <fstream>
-#include <vector>
 
-using namespace std;
 
-int main(int argc, char** argv)
-{
-    if (argc != 2) { cout << "There aren't any arguments or there are more arguments than we expect."; }
-    else
-    {
-        ifstream file(argv[1]);
-
-        //Извлекаем h0
-        string str;
-        getline(file, str);
-        float h0 = stof(str);
-
-        //Извлекаем vx и vy
-        string str1; string str2;
-        getline(file, str);
-        int i = 0; //Счетчик
-        while (str[i] != char(32)) { str1 += str[i]; i += 1; }
-        i += 1;
-        while (i < str.size()) { str2 += str[i]; i += 1; }
-        float vx = stof(str1); float vy = stof(str2);
-        
-        //Создадим динамические массивы
-        int n = 0; //Размер обоих массивов
-        vector<float> xk = { 0 }; vector<float> hk = { 0 };
-        while (getline(file, str))
-        {
-            n = n + 1;
-            str1 = ""; str2 = "";
-            i = 0; //Счетчик
-            while (str[i] != char(32)) { str1 += str[i]; i += 1; }
-            i += 1;
-            while (i < str.size()) { str2 += str[i]; i += 1; }
-            xk.push_back(stof(str1)); hk.push_back(stof(str2));
+int main(int argc, char** argv) {
+    double y_0 = 0, v_x = 0, v_y = 0, g = 9.81, answer = 0;
+    double check, tmp, t_collision;
+    int direction = 1;
+    int n = 0;
+    std::ifstream in_prev(R"(C:\Users\Thomas_Maddison\CLionProjects\Homework2\in.txt)");
+    if (in_prev.is_open()){
+        in_prev >> y_0;
+        in_prev >> v_x;
+        in_prev >> v_y;
+        double t_end = (v_y+sqrt(v_y*v_y+2*g*y_0))/g;
+        while ((!in_prev.eof()) && (check <= v_x*t_end)) {
+            in_prev >> check;
+            in_prev >> tmp;
+            n++;
         }
-        
-        //Теперь физика
-        float g = 9.81;
-        int gran = 0; //Будущий ответ, пока примем за 0 (номер перегородки)
-
-        float x = 0; float y = h0; //Текущее положение, в цикле будем обновлять
-        bool flag = 0; //Показатель столкновения со стенкой
-        
-        while (true)
-        {
-            flag = 0;
-
-            if (vx == 0) { break; }
-            
-            if (vx > 0) { //Идем слева направо
-                for (int i = gran + 1; i < xk.size(); i++) {
-                    if ((y + vy / vx * (xk[i] - x) - g / (2 * vx * vx) * (xk[i] - x) * (xk[i] - x)) >= 0 && (y + vy / vx * (xk[i] - x) - g / (2 * vx * vx) * (xk[i] - x) * (xk[i] - x)) <= hk[i]) {
-                        flag = 1;
-                        y = y + vy / vx * (xk[i] - x) - g / (2 * vx * vx) * (xk[i] - x) * (xk[i] - x);
-                        vy = vy - g * (xk[i] - x) / vx;
-                        vx = -vx;
-                        gran = i;
-                        x = xk[i];
-                    }
-                    if (flag == 1) { break; }
-                }
-                if (y == 0) { break; }
-            }
-
-            else {
-                if (vx < 0) { //Идем справа налево
-                    if (x == 0 || x == xk[1]) { gran = 0; break; }
-                    else {
-                        for (int i = gran - 1; i > 0; i--) {
-                            if ((y + vy / vx * (xk[i] - x) - g / (2 * vx * vx) * (xk[i] - x) * (xk[i] - x) >= 0) && (y + vy / vx * (xk[i] - x) - g / (2 * vx * vx) * (xk[i] - x) * (xk[i] - x)) <= hk[i]) {
-                                flag = 1;
-                                y = y + vy / vx * (xk[i] - x) - g / (2 * vx * vx) * (xk[i] - x) * (xk[i] - x);
-                                vy = vy - g * (xk[i] - x) / vx;
-                                vx = -vx;
-                                gran = i;
-                                x = xk[i];
-                            }
-                            if (flag == 1) { break; }
-                        }
-                        if (y == 0) { break; }
-                    }
-                }
-            }
-
-            bool flag1 = 0;
-
-            if (flag == 0) { //То есть столкновения не произошло
-                if (vx > 0) {
-                    if (gran != xk.size() - 1) {
-                        for (int i = gran; i < xk.size() - 1; i++) {
-                            if ((y + vy / vx * (xk[i] - x) - g / (2 * vx * vx) * (xk[i] - x) * (xk[i] - x)) * (y + vy / vx * (xk[i + 1] - x) - g / (2 * vx * vx) * (xk[i + 1] - x) * (xk[i + 1] - x)) < 0) {
-                                gran = i;
-                                flag1 = 1;
-                                break;
-                            }
-                        }
-                        if (flag1 == 0) { gran = xk.size() - 1; }
-                    }
-                    else { gran = xk.size() - 1; }
-                }
-
-                if (vx < 0) {
-                    if (xk.size() > 2) {
-                        for (int i = gran; i > 1; i--) {
-                            if ((y + vy / vx * (xk[i] - x) - g / (2 * vx * vx) * (xk[i] - x) * (xk[i] - x)) * (y + vy / vx * (xk[i - 1] - x) - g / (2 * vx * vx) * (xk[i - 1] - x) * (xk[i - 1] - x)) < 0) {
-                                gran = i - 1;
-                                flag1 = 1;
-                                break;
-                            }
-                        }
-                        if (flag1 == 0) { gran = 0; }
-                    }
-                }
-                
-                break;
-            }
-            
-        }
-
-        cout << gran;
     }
-    
-    return 0;
+    in_prev.close();
+    auto*X = new double[n];
+    auto*Y = new double[n];
+
+    std::ifstream in(R"(C:\Users\Thomas_Maddison\CLionProjects\Homework2\in.txt)");
+    if (in.is_open()){
+        in >> y_0;
+        in >> v_x;
+        in >> v_y;
+        int i = 0;
+        double t_end = (v_y+sqrt(v_y*v_y+2*g*y_0))/g;
+        while ((!in.eof()) && (X[i - 1] <= v_x*t_end)) {
+            in >> X[i];
+            in >> Y[i];
+            i++;
+        }
+    }
+    in.close();
+
+    double A_x = v_x, A_y = v_y, B_x = 0, B_y = y_0;
+    double t_end = (v_y+sqrt(v_y*v_y+2*g*y_0))/g;
+
+    for (int i = 0; (i >= 0) && (i <= n - 1); i = i + direction){
+        t_collision = (X[i] - B_x)/A_x;
+        if (t_collision <= t_end) {
+            if ((-g * t_collision * t_collision/2 + A_y * t_collision + B_y) <= Y[i]) {
+                B_x = 2*A_x*t_collision + B_x;
+                A_x = -A_x;
+                direction = (-1)*direction;
+            }
+        }else{
+            break;
+        }
+    }
+
+    double coordinate_final = A_x*t_end + B_x;
+
+    for (int i = 0; i <= n - 2; i++){
+        if ((coordinate_final >= X[i]) && (coordinate_final <= X[i + 1])){
+            answer = i + 1;
+            break;
+        }
+    }
+
+    if (coordinate_final > X[n - 1]){
+        answer = n;
+    }
+
+    if (coordinate_final < X[0]){
+        answer = 0;
+    }
+
+    delete[] X;
+    delete[] Y;
+    std::cout << answer << std::endl;
 }
